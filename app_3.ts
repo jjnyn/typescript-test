a1ef032e2299aece4c042668b99e4c32e370e812
Yuan <jiyuan@c02yr12nlvdm.agoda.local>
2019-09-06 18:02:10 +0700

    Add SystemJS

--- app_3.ts
+++ app_3.ts
@@ -1,5 +1,6 @@
 import {PI, calculateCircumference} from "./math/circle";
+import {calculateRectangle} from "./math/rectangle";
 
 console.log(PI);
 console.log(calculateCircumference(10));
-//console.log();
+console.log(calculateRectangle(20,50));
