5ea819ee680aa3e3f202fe302d9cbb3e48a9fab7
Jiajen Yuan <jiajenyuan@Jiajens-Air.true.true>
2019-09-07 11:08:15 +0700

    interface inheritence

--- app_4.js
+++ app_4.js
@@ -28,3 +28,16 @@ myPerson.firstName = "Maximilian";
 myPerson.lastName = "Anything";
 greet(myPerson);
 myPerson.greet(myPerson.lastName);
+var myDoubleFunction;
+myDoubleFunction = function (value1, value2) {
+    return (value1 + value2) * 2;
+};
+console.log(myDoubleFunction(10, 20));
+var oldPerson = {
+    age: 27,
+    firstName: "Max",
+    greet: function (lastName) {
+        console.log("Hello");
+    }
+};
+console.log(oldPerson);
--- app_4.ts
+++ app_4.ts
@@ -39,3 +39,36 @@ myPerson.firstName = "Maximilian";
 myPerson.lastName = "Anything";
 greet(myPerson);
 myPerson.greet(myPerson.lastName);
+
+
+// Function types
+
+
+interface DoubleValueFunc {
+    (number1: number, number2: number):number;
+}
+
+let myDoubleFunction: DoubleValueFunc;
+myDoubleFunction = function(value1: number, value2: number) {
+    return (value1+value2) * 2;
+}
+
+
+
+console.log(myDoubleFunction(10,20));
+
+// interface inheritence 
+
+interface AgedPerson extends NamedPerson {
+    age: number;
+}
+
+const oldPerson: AgedPerson = {
+    age: 27,
+    firstName: "Max",
+    greet(lastName: string) {
+        console.log("Hello");
+    }
+}
+
+console.log(oldPerson);
