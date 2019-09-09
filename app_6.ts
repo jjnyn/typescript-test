d8e24d420211668cd6a74d6c1a8b9247fba603ad
Yuan <jiyuan@c02yr12nlvdm.agoda.local>
2019-09-09 09:37:27 +0700

    decorator

---
+++ app_6.ts
@@ -0,0 +1,34 @@
+function logged(constructorFn: Function) {
+    console.log(constructorFn);
+}
+
+@logged
+class Person {
+    constructor() {
+        console.log("Hi!");
+    }
+}
+
+// Factory
+function logging(value: boolean) {
+    return value ? logged : null;
+}
+
+@logging(true)
+class Car {
+    
+}
+
+// Advanced
+function printable(constructorFn: Function) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    }
+}
+
+@printable
+class Plant {
+    name = "Green Plant";
+}
+const plant = new Plant();
+ (<any>plant).print();
