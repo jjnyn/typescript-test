696270305188241ba4e4dbc552c1bdd4b4f30213
Yuan <jiyuan@c02yr12nlvdm.agoda.local>
2019-09-09 10:55:30 +0700

    parameter decorator

--- app_6.js
+++ app_6.js
@@ -4,6 +4,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function logged(constructorFn) {
     console.log(constructorFn);
 }
@@ -53,33 +56,58 @@ function editable(value) {
     return function (target, propName, descriptor) {
         descriptor.writable = value;
     };
-}
-function overwritable(value) {
-    return function (target, propName) {
-        var newDescriptor = {
-            writable: value
+    function overwritable(value) {
+        return function (target, propName) {
+            var newDescriptor = {
+                writable: value
+            };
+            return newDescriptor;
         };
-        return newDescriptor;
-    };
-}
-var Project = /** @class */ (function () {
-    function Project(name) {
-        this.projectName = name;
     }
-    Project.prototype.calcBudget = function () {
-        console.log(1000);
+    var Project = /** @class */ (function () {
+        function Project(name) {
+            this.projectName = name;
+        }
+        Project.prototype.calcBudget = function () {
+            console.log(1000);
+        };
+        __decorate([
+            overwritable(false)
+        ], Project.prototype, "projectName");
+        __decorate([
+            editable(false)
+        ], Project.prototype, "calcBudget");
+        return Project;
+    }());
+    var project = new Project("Super Project");
+    project.calcBudget();
+    project.calcBudget = function () {
+        console.log(2000);
     };
-    __decorate([
-        overwritable(false)
-    ], Project.prototype, "projectName");
-    __decorate([
-        editable(false)
-    ], Project.prototype, "calcBudget");
-    return Project;
-}());
-var project = new Project("Super Project");
-project.calcBudget();
-project.calcBudget = function () {
-    console.log(2000);
-};
-project.calcBudget();
+    project.calcBudget();
+    // Parameter Decorator
+    function printInfo(target, methodName, paramIndex) {
+        console.log("Target", target);
+        console.log("methodName", methodName);
+        console.log("paramIndex", paramIndex);
+    }
+    var Course = /** @class */ (function () {
+        function Course(name) {
+            this.name = name;
+        }
+        Course.prototype.printStudentNumbers = function (mode, printAll) {
+            if (printAll) {
+                console.log(10000);
+            }
+            else {
+                console.log(2000);
+            }
+        };
+        __decorate([
+            __param(1, printInfo)
+        ], Course.prototype, "printStudentNumbers");
+        return Course;
+    }());
+    var course = new Course("Super Course");
+    course.printStudentNumbers("anything", true);
+}
--- app_6.ts
+++ app_6.ts
@@ -36,11 +36,10 @@ const plant = new Plant();
 
  // Method Decorator
  //Property decorator
- function editable(value: boolean) {
-     return function(target: any, propName: string, descriptor: PropertyDescriptor) {
-        descriptor.writable = value;
-     }
- }
+function editable(value: boolean) {
+        return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+    descriptor.writable = value;
+    }
 
  function overwritable(value: boolean){
     return function(target: any, propName: string): any {
@@ -51,23 +50,49 @@ const plant = new Plant();
     }
  }
 
- class Project {
-     @overwritable(false)
-     projectName: string;
+class Project {
+    @overwritable(false)
+    projectName: string;
 
-     constructor(name: string) {
-         this.projectName = name;
-     }
+    constructor(name: string) {
+        this.projectName = name;
+    }
 
-     @editable(false)
+    @editable(false)
     calcBudget() {
         console.log(1000);
     }
- }
+}
 
- const project = new Project("Super Project");
- project.calcBudget();
- project.calcBudget = function() {
-     console.log(2000);
- };
- project.calcBudget();
+const project = new Project("Super Project");
+project.calcBudget();
+project.calcBudget = function() {
+    console.log(2000);
+};
+project.calcBudget();
+
+
+
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+    console.log("Target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("paramIndex: ", paramIndex);
+}
+
+class Course {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+        if (printAll) {
+            console.log(10000);
+        } else {
+            console.log(2000);
+        }
+    }
+}
+const course = new Course("Super Course");
